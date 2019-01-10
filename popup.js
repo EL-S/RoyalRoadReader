93feef351787a6d07d07003723e0b6cb95907b6f
EL-S <31227083+EL-S@users.noreply.github.com>
2019-01-10 18:56:54 +1100

    Attempt at Storage Implementation
    Attempt for a persistent state when enabled or disabled (not working currently)

--- popup.js
+++ popup.js
@@ -1,1 +1,9 @@
-//empty for now
+
+
+// Read it using the storage API
+chrome.storage.sync.get('status', function(value_name) {
+    console.log(value_name);
+    message('Settings retrieved', value_name);
+});
+document.getElementById("formatpage").innerHTML = "test";
+document.getElementById("formatpage").innerHTML = value_name;
--- scripts/extension.js
+++ scripts/extension.js
@@ -1,6 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
     var button = document.getElementById('formatpage');
+
     button.addEventListener('click', function () {
+        
+        var status_str = document.getElementById("formatpage").innerHTML;
+        var status = "";
+
+        if (status_str == "Enable") {
+            status = "enabled";
+            status_str = "Disable" //the new button text
+        } else if (status_str == "Disable") {
+            status = "disabled";
+            status_str = "Enable"; //the new button text
+        } else {
+            status = "disabled";
+            status_str = "Enable"; //the new button text
+        }
+    
+        // Save it using the Chrome extension storage API.
+        chrome.storage.sync.set({'status': status, 'status_str': status_str}, function() {
+            console.log('Settings saved');
+        });
+        console.log('Settings test');
+        document.getElementById("formatpage").innerHTML = status_str;     
+
         var text = "test";
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
             chrome.tabs.sendMessage(tabs[0].id, {data: text}, function(response) {
